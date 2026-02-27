1de17c0bbff4e579f5ad60b74b5197664e993a8c
Thibaut <thibaut.mottet@gmail.com>
2026-02-27 12:22:37 +0100

    ci: test error and autofix sentry-autofix.yml

--- server/routes/dashboard/index.get.ts
+++ server/routes/dashboard/index.get.ts
@@ -2,5 +2,6 @@ import { DashboardQuery } from '~/domain/dashboard/query'
 
 export default defineEventHandler(async () => {
   const dashboard = await DashboardQuery.get()
+  throw new Error('test error with sentry-autofix')
   return { status: 200, data: dashboard }
 })
