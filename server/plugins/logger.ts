cd3b75c5bf166e37a1dc387a51fbf6ccdd27c19c
Thibaut <thibaut.mottet@gmail.com>
2026-03-05 17:35:58 +0100

    fix(logger): disable log for health

--- server/plugins/logger.ts
+++ server/plugins/logger.ts
@@ -4,9 +4,11 @@ const log = createLogger('http')
 
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('request', (event) => {
+    if (event.path === '/health') return
     log.info('on request', event.path)
   })
   nitroApp.hooks.hook('beforeResponse', (event, { body }) => {
+    if (event.path === '/health') return
     log.info('on response', event.path, { body })
   })
   nitroApp.hooks.hook('error', (error) => {
